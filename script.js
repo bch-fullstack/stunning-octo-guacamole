da55e0eeea623c8c38385f9714b4aef28d7c0800
Tien Hoang Pham <tienhoang.pham@bc.fi>
2021-03-04 12:36:45 +0200

    API call to randomUser API and render to DOM

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-(function(){
-    document.addEventListener('DOMContentLoaded', () => {
-        alert('Loaded!')
-    })
+(function () {
+    document.addEventListener('DOMContentLoaded', executeScript)
+
+    const users = [{
+        "name": {
+            "title": "Mr",
+            "first": "Alexander",
+            "last": "Kristensen"
+        }, 
+        "email": "alexander.kristensen@example.com", 
+        "phone": "98708762", 
+        "picture": {
+            "large": "https://randomuser.me/api/portraits/men/8.jpg",
+            "medium": "https://randomuser.me/api/portraits/med/men/8.jpg",
+            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/8.jpg"
+        },
+    }];
+
+    function executeScript() {
+        fetch('https://randomuser.me/api/?results=100')
+            .then(resp => resp.json())
+            .then(json => {
+                // { results: [], info: {}} 
+                json.results.forEach(addUser)
+            })
+    }
+
+    function addUser(user) {
+        const newEl = document.createElement('div');
+        newEl.className = 'user';
+        newEl.innerHTML = `
+            <div class="profile-image">
+                <img src="${user.picture.large}"/>
+            </div>
+            <div class="first-name">${user.name.first}</div>
+            <div class="last-name">${user.name.last}</div>
+            <div class="email">${user.email}</div>
+            <div class="phone">${user.phone}2</div>
+        `;
+        document.getElementById('user-list').appendChild(newEl);
+    }
 })();
 
